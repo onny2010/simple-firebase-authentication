ed16d16f3b4fe9a8e7af55a79fd06dd7798ed762
Tazmin_Onny <onny2010@gmail.com>
2021-10-14 01:04:28 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjQQuEOIw9UoXTueFtXnROtIUVBBM3vwM",
+    authDomain: "simple-firebase-authenti-1f748.firebaseapp.com",
+    projectId: "simple-firebase-authenti-1f748",
+    storageBucket: "simple-firebase-authenti-1f748.appspot.com",
+    messagingSenderId: "490472244692",
+    appId: "1:490472244692:web:8b3e99972d31bfc392c241"
+};
+
+export default firebaseConfig;
